be02431f61f0548b075c458442123d818225a283
hui811116 <huan1456@purdue.edu>
2022-12-19 20:06:48 +0800

    revising sto script

--- run_opt_sto.py
+++ run_opt_sto.py
@@ -8,6 +8,7 @@ import algorithm as alg
 import dataset as dt
 import evaluation as ev
 import datetime
+import copy
 
 parser = argparse.ArgumentParser()
 parser.add_argument("method",choices=["admmgd","tfadmmgd","logadmm","gdbaseline","loggd",'logdrs'])
@@ -17,7 +18,7 @@ parser.add_argument("--convthres",type=float,default=1e-6,help="convergence thre
 parser.add_argument("--nrun",type=int,default=10,help="number of trail of each simulation")
 parser.add_argument("--ss_init",type=float,default=1e-2,help="step size initialization")
 parser.add_argument("--ss_scale",type=float,default=0.25,help="step size scaling")
-parser.add_argument("--output",type=str,default="unsu_mv_output",help="output filename")
+parser.add_argument("--output_dir",type=str,default="unsu_sto_results",help="output filename")
 parser.add_argument("--seed",type=int,default=None,help="random seed for reproduction")
 parser.add_argument("--ny",type=int,default=2,help="number of uniform hidden labels")
 parser.add_argument("--nb",type=int,default=2,help="number of blocks for observations")
@@ -46,10 +47,7 @@ print("Hx1x2={:.4f}".format(ut.calcEnt(prob_joint)))
 prob_cond = data_dict["p_cond"]
 px1cx2 = prob_cond[0]
 px2cx1 = prob_cond[1]
-# TODO: what if |Z| neq |Y|
-#nz = len(data_dict["py"]) # FIXME: for "condindp" dataset, |Y|=3
 
-#gamma_range = np.geomspace(1,args.gamma_max,num=args.gamma_num)
 gamma_range = np.geomspace(args.gamma_min,args.gamma_max,num=args.gamma_num)
 
 alg_dict = {
@@ -77,7 +75,6 @@ else:
 
 encoder_dict = {}
 
-#nz_set = [nz]
 nz_set = np.arange(2,len(px1)*len(px2)+1,1)
 res_all = np.zeros((len(gamma_range)*args.nrun*len(nz_set),12)) # gamma, nidx, niter, conv,nz, entz, mizx1,mizx2,cmizx1cx2, cmizx2cx1, loss, cmix1x2cz
 rec_idx = 0
@@ -85,8 +82,6 @@ for gidx ,gamma in enumerate(gamma_range):
 	encoder_dict[gidx] = {}
 	for nz in nz_set:
 		for nn in range(args.nrun):
-		#for nn in range(2):
-			#out_dict = alg.admmHighDim(prob_joint,nz,gamma,args.maxiter,args.convthres,**alg_dict)
 			out_dict = algrun(prob_joint,nz,gamma,args.maxiter,args.convthres,**alg_dict)
 			tmp_result = [gamma,nn,out_dict['niter'],int(out_dict["conv"]),nz]
 				
@@ -94,13 +89,16 @@ for gidx ,gamma in enumerate(gamma_range):
 			# calculate the mutual informations
 			pz = out_dict["pz"]
 			entz = ut.calcEnt(pz)
+			# calculate ent_z|x1x2
 			pzcx1 = out_dict["pzcx1"]
 			pzcx2 = out_dict["pzcx2"]
 			# compute the joint encoder
 			pzcx1x2 = out_dict['pzcx1x2'] # this might not be a valid prob, but is forced to be valid one
-			#print(pzcx1x2)
+
 			pzx1x2 = pzcx1x2 * prob_joint[None,:,:]
+			entzcx1x2 = -np.sum(pzx1x2 * np.log(pzcx1x2))
 			cmix1x2z = ut.calcMIcond(np.transpose(pzx1x2,axes=[1,2,0]))
+
 			if args.encoder:
 				encoder_dict[gidx][nn] = pzcx1x2
 			# take the maximum element
@@ -111,27 +109,34 @@ for gidx ,gamma in enumerate(gamma_range):
 			cmizx2cx1 = ut.calcMIcond(np.transpose(pzcx1x2 * prob_joint[None,:,:],(0,2,1)))
 			cmix1x2cz = ut.calcMIcond(np.transpose(pzcx1x2 * prob_joint[None,:,:],(1,2,0)))
 			# loss calculation
-			joint_mi = ut.calcMI(np.reshape(pzcx1x2 * prob_joint[None,:,:],(nz,np.prod(prob_joint.shape))))
+			joint_mi = entz - entzcx1x2
 			tmp_loss = (1+gamma)*joint_mi - gamma * mizx1 - gamma * mizx2
 			tmp_result += [entz,mizx1,mizx2,cmizx1cx2,cmizx2cx1,tmp_loss,cmix1x2cz]
 				
 			res_all[rec_idx,:] = np.array(tmp_result)
 			print("gamma,{:.4f},ntrial,{:},nz,{:},convergence,{:},niter,{:},I(X1,X2;Z),{:.6f},H(Z),{:.6f},tmp_loss,{:.5f},I(X1;X2|Z),{:.5f}".format(gamma,nn,nz,int(out_dict["conv"]),out_dict["niter"],joint_mi,entz,tmp_loss,cmix1x2cz))
 			rec_idx += 1
-		#sys.exit()
-		#print("simulated gamma:{:.4f}, nz:{:}, conv_rate:{:.4f}, max_element:{:.4f}, min_loss:{:.5f}".format(gamma,nz,conv_cnt/args.nrun,avg_test,min_loss))
 
 timenow= datetime.datetime.now()
-ymd_tex = "{:04}{:02}{:02}".format(timenow.year,timenow.month,timenow.day)
-hm_tex  = "h{:02}m{:02}".format(timenow.hour,timenow.minute)
-md_dir_name = "{:}_{:}_{:}".format(args.output,ymd_tex,hm_tex)
+# result directory
+d_cwd = os.getcwd()
+d_save_dir = os.path.join(d_cwd,args.output_dir)
+os.makedirs(d_save_dir,exist_ok=True)
+
+repeat_cnt = 0
+safe_savename_base = "sto_{:}_y{:}b{:}_cr{:.4f}_nzall_{:}".format(args.method,args.ny,args.nb,args.corr,timenow.strftime("%Y%m%d"))
+safe_savename = copy.copy(safe_savename_base)
+while os.path.isfile(os.path.join(d_save_dir,safe_savename)):
+	repeat_cnt +=1
+	safe_savename = "{:}_{:}".format(safe_savename_base,repeat_cnt)
+
 # saving the results, numpy array
-with open(args.output+".npy","wb") as fid:
+with open(os.path.join(d_save_dir,safe_savename+".npy"),"wb") as fid:
 	np.save(fid,res_all)
 # saving the encoders
 if args.encoder:
-	with open(args.output+"_encoders.pkl","wb") as fid:
+	with open(os.path.join(d_save_dir,safe_savename+"_encoders.pkl"),"wb") as fid:
 		pickle.dump(encoder_dict,fid)
 # saving the configuration in case of error
-with open(args.output+"_config.pkl","wb") as fid:
+with open(os.path.join(d_save_dir,safe_savename+"_config.pkl"),"wb") as fid:
 	pickle.dump(argsdict,fid)
